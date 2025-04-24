87f1c96a5fcc94e2e24acdc115468f99e9623cb8
Paul Nhlapo <62051532+paul-nhlapo@users.noreply.github.com>
2025-04-24 13:28:38 +0200

    Function to add all the Positive Even Values

--- Scheme_Practice.scm
+++ Scheme_Practice.scm
@@ -230,3 +230,16 @@
 (display (getPositiveEvenValues '(2 -2 4 6 5 0 5 -9 10 12))); ; Expected output: (2 4 6 10 12)
 (display (getPositiveEvenValues '(1 3 5 -7 -9))); ; Expected output: ()
 (display (getPositiveEvenValues '())); ; Expected output: ()
+
+
+;Now a Function to add all positive even values in a list. 
+;Return 0 is the list is empty
+(define (addPositiveEvenValues list)
+    (cond
+        ((null? list) 0)
+        ((and (> (car list) 0) (even? (car list))) (+ (car list) (addPositiveEvenValues(cdr list))))
+        (else (addPositiveEvenValues(cdr list)))
+    )
+)
+
+(display (addPositiveEvenValues '(1 2 3 4 5 6 -8 -2)))
